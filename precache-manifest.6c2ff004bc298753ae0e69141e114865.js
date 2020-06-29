c83a74ff65b44a078f13568f84c9e4f44668a3a9
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-06-29 11:32:57 -0300

    Updates

---
+++ precache-manifest.6c2ff004bc298753ae0e69141e114865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375c0ac1832d68dcaedbe90a67d7139f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e825b2b22c59e62de22d",
+    "url": "/monsters-rolodex/static/css/main.493518f0.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e825b2b22c59e62de22d",
+    "url": "/monsters-rolodex/static/js/main.dea33bec.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
